052a846c5f91966e44f948ecbe11c7b68d39884b
soheldeveloper <sohelrananayanpur@gmail.com>
2022-08-02 00:05:44 +0600

    updated answer

--- Q3/sortArrayValue.php
+++ Q3/sortArrayValue.php
@@ -1,20 +1,32 @@
 <?php
 
-function sortArray($input)
-{
-$removeDuplicate = array_unique($input);
 
-$positiveValue = array_filter($removeDuplicate, function ($x){return $x>0;});
-$negativeValue = array_filter($removeDuplicate, function ($x){return $x<0;});
 
-sort($positiveValue);
-sort($negativeValue);
+function sortAscArray($input){
+    $uniqueArray = array();
 
-return array_merge($negativeValue, $positiveValue);
+    foreach ($input as $item) {
+        if (in_array($item, $uniqueArray)){
+            continue;
+        }else{
+            array_push($uniqueArray, $item);
+        }
+    }
+
+    for ($i = 0; $i < count($uniqueArray); ++$i){
+        for ($j = $i+1; $j < count($uniqueArray); ++$j){
+            if ($uniqueArray[$i] > $uniqueArray[$j]){
+                $temp = $uniqueArray[$i];
+                $uniqueArray[$i] = $uniqueArray[$j];
+                $uniqueArray[$j] = $temp;
+            }
+        }
+    }
+    print_r($uniqueArray);
 }
 
 $input = [-2, 12, 45, -9, 1, 5, 12, -2];
-print_r(sortArray($input));
+sortAscArray($input);
 
 
 
